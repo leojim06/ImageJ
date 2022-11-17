bb4ca874a9ad1fb04b0e9a971a993951ba0b3703
o.jimenez-at-285254948890 <leojim06@gmail.com>
2022-11-17 12:19:50 -0500

    eliminación duplicate code block 46

--- src/ij/gui/Roi.java
+++ src/ij/gui/Roi.java
@@ -1074,6 +1074,35 @@ public class Roi extends Object implements Cloneable, java.io.Serializable, Iter
         oldWidth = width;
         oldHeight = height;
     }
+    
+    private void activeHandleSwitch(int x2, int y2, int xc, int yc) {
+        switch (activeHandle) {
+            case 0:
+                x = x2 - width;
+                y = y2 - height;
+                break;
+            case 1:
+                x = xc - width / 2;
+                y = y2 - height;
+                break;
+            case 2:
+                y = y2 - height;
+                break;
+            case 3:
+                y = yc - height / 2;
+                break;
+            case 5:
+                x = xc - width / 2;
+                break;
+            case 6:
+                x = x2 - width;
+                break;
+            case 7:
+                y = yc - height / 2;
+                x = x2 - width;
+                break;
+        }
+    }
 
     protected void moveHandle(int sx, int sy) {
         double asp;
@@ -1208,32 +1237,8 @@ public class Roi extends Object implements Cloneable, java.io.Serializable, Iter
                 height = 1;
                 y = y2 = yc;
             }
-            switch (activeHandle) {
-                case 0:
-                    x = x2 - width;
-                    y = y2 - height;
-                    break;
-                case 1:
-                    x = xc - width / 2;
-                    y = y2 - height;
-                    break;
-                case 2:
-                    y = y2 - height;
-                    break;
-                case 3:
-                    y = yc - height / 2;
-                    break;
-                case 5:
-                    x = xc - width / 2;
-                    break;
-                case 6:
-                    x = x2 - width;
-                    break;
-                case 7:
-                    y = yc - height / 2;
-                    x = x2 - width;
-                    break;
-            }
+            activeHandleSwitch(x2, y2, xc, yc);
+            
             if (center) {
                 x = xc - width / 2;
                 y = yc - height / 2;
@@ -1247,32 +1252,8 @@ public class Roi extends Object implements Cloneable, java.io.Serializable, Iter
                 height = (int) Math.rint((double) width / asp);
             }
 
-            switch (activeHandle) {
-                case 0:
-                    x = x2 - width;
-                    y = y2 - height;
-                    break;
-                case 1:
-                    x = xc - width / 2;
-                    y = y2 - height;
-                    break;
-                case 2:
-                    y = y2 - height;
-                    break;
-                case 3:
-                    y = yc - height / 2;
-                    break;
-                case 5:
-                    x = xc - width / 2;
-                    break;
-                case 6:
-                    x = x2 - width;
-                    break;
-                case 7:
-                    y = yc - height / 2;
-                    x = x2 - width;
-                    break;
-            }
+            activeHandleSwitch(x2, y2, xc, yc);
+            
             if (center) {
                 x = xc - width / 2;
                 y = yc - height / 2;
